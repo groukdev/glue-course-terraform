4ccfd8c29bc7463b70e729dba4544f82ec732f27
Gabriel Gama <groukdev@gmail.com>
2023-08-14 11:46:00 +0100

    update s3.tf to create the directory structure

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,5 @@
-resource "aws_s3_bucket" "example" {
+# Create main bucket with some tags
+resource "aws_s3_bucket" "main" {
   bucket = "${local.prefix}-glue-full-course"
 
   tags = merge(
@@ -8,4 +9,24 @@ resource "aws_s3_bucket" "example" {
       Env  = "Dev"
     }
   )
+}
+
+# Create a folder path variable to be created
+variable "s3_folders" {
+  type        = list(string)
+  description = "The list of S3 folders to create"
+  default = [
+    "data/customers_database/customers_csv/20211230",
+    "temp-dir",
+    "scripts"
+  ]
+}
+
+# Create the folders inside the main bucket
+resource "aws_s3_object" "directory_structure" {
+  for_each = toset(var.s3_folders)
+
+  bucket       = aws_s3_bucket.main.bucket
+  key          = "${each.value}/"
+  content_type = "application/x-directory"
 }
